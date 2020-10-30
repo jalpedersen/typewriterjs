8eff698d2d957dbada189c8b6ac0cd5a8fe41b37
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2020-10-30 09:19:04 +0100

    insert css from style-loader at top, so we can override the styles

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,29 @@ module.exports = {
             },
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            insert: function insertAtTop(element) {
+                                var parent = document.querySelector('head');
+                                var lastInsertedElement =
+                                    window._lastElementInsertedByStyleLoader;
+
+                                if (!lastInsertedElement) {
+                                    parent.insertBefore(element, parent.firstChild);
+                                } else if (lastInsertedElement.nextSibling) {
+                                    parent.insertBefore(element, lastInsertedElement.nextSibling);
+                                } else {
+                                    parent.appendChild(element);
+                                }
+
+                                window._lastElementInsertedByStyleLoader = element;
+                            },
+                        },
+                    },
+                    'css-loader',
+                ],
             },
         ],
     },
